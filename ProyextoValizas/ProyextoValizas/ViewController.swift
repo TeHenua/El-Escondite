8c784595a0beed3101a3fca9f30942b8470b89c4
Carlos <carlos95egibide@gmail.com>
2018-02-01 20:37:55 +0100

    Primer Commit real de ProyextoValizas

---
+++ ProyextoValizas/ProyextoValizas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyextoValizas
+//
+//  Created by Carlos Hernández on 31/1/18.
+//  Copyright © 2018 Carlos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
