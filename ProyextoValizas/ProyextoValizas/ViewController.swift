fcf1f234ab184efb22eea178fa1f4f8a7440dac2
Unknown <aritz.martinezh@gmail.com>
2018-02-08 22:08:41 +0100

    Mantener boton pulsado

--- ProyextoValizas/ProyextoValizas/ViewController.swift
+++ ProyextoValizas/ProyextoValizas/ViewController.swift
@@ -15,19 +15,35 @@ class ViewController: UIViewController, SRCountdownTimerDelegate {
     @IBOutlet weak var contadorSOS: SRCountdownTimer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    @IBAction func SOS(_ sender: Any) {
-        contadorSOS.start(beginingValue: 3)
+        
         contadorSOS.delegate = self
         
     }
+    
+    
+    @IBAction func mantenerPulsado(_ sender: UIButton) {
+        //este es el que cancela la accion
+        contadorSOS.start(beginingValue: 3)
+        contadorSOS.pause()
 
+        
+        
+    }
+    
+    @IBAction func cancelarPulsado(_ sender: UIButton) {
+        
+        //este boton es el que inicia
+         contadorSOS.start(beginingValue: 3)
+        
+    }
     
     
     
     func timerDidEnd() {
-        print("Tiempo!!!")
+        
+        //envia el aviso
+        print("SOS!!!!")
+        
     }
     
     override func didReceiveMemoryWarning() {
